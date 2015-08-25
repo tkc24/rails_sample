5883bbf9b2190627744dda3fdfe7b7bea1813735
vagrant <vagrant@vagrant-centos65.vagrantup.com>
2015-08-25 07:50:07 +0000

    シードデータ投入設定

---
+++ db/seeds/development/staff_members.rb
@@ -0,0 +1,9 @@
+StaffMember.create!(
+  email: 'nishi@example.com',
+  family_name: '西',
+  given_name: '武史',
+  family_name_kana: 'ニシ',
+  given_name_kana: 'タケシ',
+  password: 'password',
+  start_date: Date.today
+)
