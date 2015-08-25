1e4c4d2fe56fe3922142bab82fe217a4368e7f5e
Takeshi Nishi <nishi@woolop.co.jp>
2015-08-25 22:42:48 +0900

    アクション名修正

--- config/routes.rb
+++ config/routes.rb
@@ -58,14 +58,14 @@ Rails.application.routes.draw do
     root 'top#index'
     get 'login' => 'sessions#new', as: :login
     post 'session' => 'sessions#create', as: :session
-    delete 'session' => 'session#destroy'
+    delete 'session' => 'sessions#destroy'
   end
 
   namespace :admin do
     root 'top#index'
     get 'login' => 'sessions#new', as: :login
     post 'session' => 'sessions#create', as: :session
-    delete 'session' => 'session#destroy'
+    delete 'session' => 'sessions#destroy'
   end
 
   namespace :customer do
