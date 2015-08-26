4f66f93524089a15a51bdd1f2e959762492a233f
Takeshi Nishi <nishi@woolop.co.jp>
2015-08-26 13:49:19 +0900

    suspendedユーザーログイン時処理修正

--- app/controllers/staff/sessions_controller.rb
+++ app/controllers/staff/sessions_controller.rb
@@ -14,9 +14,14 @@ class Staff::SessionsController < Staff::Base
       staff_member = StaffMember.find_by(email_for_index: @form.email.downcase)
     end
     if Staff::Authenticator.new(staff_member).authenticate(@form.password)
-      flash.notice = "ログインしました。"
-      session[:staff_member_id] = staff_member.id
-      redirect_to :staff_root
+      if staff_member.suspended?
+        flash.now.alert = "アカウントが停止されています。"
+        render action: 'new'
+      else
+        flash.notice = "ログインしました。"
+        session[:staff_member_id] = staff_member.id
+        redirect_to :staff_root
+      end
     else
       flash.now.alert = "ログインに失敗しました。"
       render action: 'new'
