c21317d00dd12a1d0c0a4fded2d90095a5f01a80
Takeshi Nishi <nishi@woolop.co.jp>
2015-08-26 13:21:55 +0900

    ログイン・ログアウト時のflashメッセージ処理追加

--- app/controllers/staff/sessions_controller.rb
+++ app/controllers/staff/sessions_controller.rb
@@ -14,14 +14,17 @@ class Staff::SessionsController < Staff::Base
       staff_member = StaffMember.find_by(email_for_index: @form.email.downcase)
     end
     if Staff::Authenticator.new(staff_member).authenticate(@form.password)
+      flash.notice = "ログインしました。"
       session[:staff_member_id] = staff_member.id
       redirect_to :staff_root
     else
+      flash.now.alert = "ログインに失敗しました。"
       render action: 'new'
     end
   end
 
   def destroy
+    flash.notice = "ログアウトしました。"
     session.delete(:staff_member_id)
     redirect_to :staff_root
   end
