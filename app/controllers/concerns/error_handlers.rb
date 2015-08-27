5466c7c486b6e7552942b093b06d8f3569cd7ef1
Takeshi Nishi <nishi@woolop.co.jp>
2015-08-27 22:02:14 +0900

    MissingParameterエラー処理対応

--- app/controllers/concerns/error_handlers.rb
+++ app/controllers/concerns/error_handlers.rb
@@ -2,6 +2,7 @@ module ErrorHandlers
   extend ActiveSupport::Concern
   included do
     rescue_from Exception, with: :rescure500
+    rescue_from ActionController::ParameterMissing, with: :rescure400
     rescue_from ApplicationController::Forbidden, with: :rescure403
     rescue_from ApplicationController::IpAddressRejected, with: :rescure403
     rescue_from ActionController::RoutingError, with: :rescure404
@@ -9,6 +10,11 @@ module ErrorHandlers
   end
 
   private
+  def rescure400(e)
+    @exception = e
+    render 'errors/bad_request', status: 400
+  end
+
   def rescure404(e)
     @exception = e
     render 'errors/not_found', status: 404
